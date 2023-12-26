83493ae79e3cb34f1dd4af0622dcc502710f1b94
Mr Josh <mr-josh@sparklapse.com>
2023-12-26 09:05:46 +0000

    Fixed some typescript issues and removed exports

--- cli/create-dots-source/templates/source-ts/rollup.config.mjs
+++ cli/create-dots-source/templates/source-ts/rollup.config.mjs
@@ -1,17 +1,25 @@
 import svelte from "rollup-plugin-svelte";
+import preprocess from "svelte-preprocess";
 import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
 
 /** @type {import("rollup").RollupOptions} */
 export default {
-  input: "./src/index.js",
+  input: "./src/index.ts",
   output: {
     file: `./dist/source.js`,
     format: "iife",
   },
   plugins: [
     svelte({
-      preprocess: [],
+      preprocess: [
+        preprocess({
+          typescript: {
+            tsconfigFile: "./tsconfig.build.json",
+          },
+        }),
+      ],
       emitCss: false,
       compilerOptions: {
         css: "injected",
--- cli/create-dots-source/templates/source-ts/src/index.ts
+++ cli/create-dots-source/templates/source-ts/src/index.ts
@@ -1,6 +1,6 @@
 import Source, { label, transform, options } from "./Source.svelte";
 import { createSource } from "@sparklapse/dots/overlay";
 
-export const source = createSource(Source, { transform, options });
+const source = createSource(Source, { transform, options });
 
 customElements.define("source-" + label, source);
