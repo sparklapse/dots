943fff327b8742089e9a8d674d01eb71f827aed3
Mr Josh <mr-josh@sparklapse.com>
2023-12-30 04:27:50 +0000

    Updated compiled source example with v0.3.0

--- package/src/routes/overlay/editor/+page.svelte
+++ package/src/routes/overlay/editor/+page.svelte
@@ -7,10 +7,10 @@
   let sources: Sources = [];
   onMount(async () => {
     await import("./component/index.js");
-    await import("./built/source-0.2.1-m.js");
+    await import("./built/source-0.3.0.js");
     const textSource = (await customElements.whenDefined("source-text")) as DotsSource;
     const imageSource = (await customElements.whenDefined("source-image")) as DotsSource;
-    const mySource = await customElements.whenDefined("source-my-source") as DotsSource;
+    const mySource = (await customElements.whenDefined("source-my-source")) as DotsSource;
 
     sources = [
       {
@@ -33,7 +33,7 @@
       },
       {
         id: "def456",
-        label: "image 1",
+        label: "custom v0.3.0",
         tag: "source-my-source", // built
         editor: {
           locked: false,
